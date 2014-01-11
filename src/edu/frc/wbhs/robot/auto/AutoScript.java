5904996bb2998549da35149298dd8365f867e767
X54329 <bobojoe108@gmail.com>
2014-01-11 18:10:33 -0500

    begin work on autonomous scripting

---
+++ src/edu/frc/wbhs/robot/auto/AutoScript.java
@@ -0,0 +1,22 @@
+package edu.frc.wbhs.robot.auto;
+
+/**
+ *
+ * @author Brendan
+ */
+public class AutoScript {
+	// This is where we create methods we use in the autonomous mode
+	// TODO: make these methods do something
+	
+	public void autoDrive(double distance, double speed, double anglechange) {
+		//TODO: set up counting sensors and magic math for this
+	}
+	public void autoTurn(double degrees, double speed) {
+		
+	}
+	public int[] getFieldLocation() {
+		//hey, should we get color sensors?
+		return null;
+	}
+	
+}
