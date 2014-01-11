530882aa41512de2ac7db3bff644d3d88a07c5a2
Brian <Brian@briansdnmachine>
2014-01-11 11:54:05 -0500

    math added

--- src/edu/frc/wbhs/robot/math/MathHelper.java
+++ src/edu/frc/wbhs/robot/math/MathHelper.java
@@ -6,20 +6,12 @@
 
 package edu.frc.wbhs.robot.math;
 
-import java.util.Vector;
-
 /**
  *
  * @author Brian
  */
 public class MathHelper {
     
-    public static Point2D toCartesian(double rot, double mag) {
-	return new Point2D(Math.cos(rot) * mag, Math.sin(rot) * mag);
-    }
     
-    public static Vector toPolar(double x, double y) {
-	return null;//new Vector(Math.atan2(y, x), Math.hypot(x, y));
-    }
     
 }
